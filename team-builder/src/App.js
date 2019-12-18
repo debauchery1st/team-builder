80752a3d9b5fe87b6a5b9fbcd71b6388f1887b1b
debauchery1st <trevor.j.martin@gmail.com>
2019-12-18 17:12:17 -0500

    Minimum Viable Product

---
+++ team-builder/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import SignUpForm from './Form';
+
+function App() {
+
+  const [teamMembers, setTeamMembers] = useState({
+    'root@localhost': {role:'auth', name: 'root'},
+  });
+  const [formData, setFormData] = useState({name: '', role: '', email: ''});
+  const [wipe, setWipe] = useState(false);
+  
+  const onInputChange = (ev) => {
+    if (wipe) {
+      return true;
+    }
+    // live input type checking here
+    setFormData({
+      ...formData,
+      [ev.target.name]: ev.target.value,
+    });
+  }
+
+  const clearFormInfo = () => {
+    console.log('clearing form');
+    debugger
+    setWipe(true)
+    setFormData({
+      email: "",
+      name: "",
+      role: ""
+    })
+    setWipe(false)
+  }
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+      {Object.keys(teamMembers).filter(emailAddr => emailAddr !== "root@localhost").map(emailAddr => {
+        return (
+          <div className="SavedTeamMember">
+            <li>{teamMembers[emailAddr].name}</li>
+            <li>{teamMembers[emailAddr].role}</li>
+            <li>{emailAddr}</li>
+         </div>);
+      })}
+      <SignUpForm 
+        onSubmit={setTeamMembers} 
+        onChange={onInputChange} 
+        teamMembers={teamMembers}
+        info={formData}
+        clearForm={clearFormInfo}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/Form.js
@@ -0,0 +1,81 @@
+import React, { useEffect } from 'react';
+
+const formValidates = (name, email, role) => {
+    // debugger
+    if (!email.length > 3 || !email.includes('@')) {
+      alert("must have a valid email address");
+      return false;
+    }
+    if (name.length === 0) {
+      alert("must have a name");
+      return false;
+    }
+    if (!role.length > 5) {
+      alert("describe role");
+      return false;
+    }
+    // if (Object.keys(teamMembers).includes(formData.email)) {
+    //   alert("That email address has already signed up.")
+    //   return false;
+    // }
+    return true
+  }
+
+
+const SignUpForm = (props) => {
+
+    useEffect(() => {
+        console.log("mounted sign up form");
+        return () => console.log("dismounted sign up for")
+    }, []);
+
+    return (
+        <form 
+        onSubmit={(ev) => {
+            ev.preventDefault();
+            if (!formValidates(
+                props.info.name,
+                props.info.email,
+                props.info.role
+            )) {
+                return false
+            }
+            
+            props.onSubmit({
+            ...props.teamMembers,
+            [props.info.email]: {
+                name: props.info.name,
+                role: props.info.role
+            }
+            })
+            ev.currentTarget.reset();
+        }}
+        onChange={props.onChange} 
+        className="TeamMember form" 
+        action="" method="get">
+        
+        <div className="TeamMember name">
+          <label for="GET-name">Name</label>
+          <input id="GET-name" type="text" name="name"  />
+        </div>
+
+        <div className="TeamMember email">
+          <label for="GET-email">Email</label>
+          <input id="GET-email" type="text" name="email"/>
+        </div>
+
+        <div className="TeamMember role">
+          <label for="GET-role">Role</label>
+          <input id="GET-role" type="text" name="role" />
+        </div>
+
+        <div className="TeamMember save">
+          <input className="TeamMember save btn" type="submit" value="Save" />
+        </div>
+
+      </form>
+    )
+}
+
+export default SignUpForm
+
